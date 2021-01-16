8be4abc351dc71c723e737d262293fb6f2be693b
Noah Loomis <56049624+NoahMLoomis@users.noreply.github.com>
2021-01-16 17:32:52 -0500

    Finish functionality

--- js/index.js
+++ js/index.js
@@ -1,83 +1,92 @@
 const searchBtn = document.querySelector("#searchBtn");
 const searchDetails = document.querySelector("#searchBar");
 const results = document.querySelector("#movieResultList");
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-let nominatedMovies=[];
+let nominatedMovies = [];
 
+/*
+Returns false if the movie has not been nominated, and true otherwise
+*/
 
 const checkNominate = (movie) => {
     let movieAlreadyNominated = false;
     nominatedMovies.forEach(nomMovie => {
-        if (movie === nomMovie){
+        if (movie.imdbID === nomMovie.imdbID) {
             movieAlreadyNominated = true;
         }
     })
-
     return movieAlreadyNominated;
-    
+
 }
 
 const searchForMovie = (link) => {
     fetch(link)
         .then(data => data.json())
         .then(movies => {
-
             console.log(movies);
-            if (movies.Response === "False"){
-                results.innerHTML+= `${movie.Error}`;
-            }else{
-
+            if (movies.Response === "False") {
+                results.innerHTML += `${movies.Error}`;
+            } else {
                 movies.Search.forEach(movie => {
 
                     let detailsNode = document.createElement("li");
                     let btnNode = document.createElement("button");
+                    btnNode.setAttribute("class", "btn btn-success");
                     btnNode.innerHTML = "Nominate";
-                    btnNode.setAttribute("id", `${movie.Title}Add`);
+                    btnNode.setAttribute("id", `${movie.imdbID}Add`);
+
                     btnNode.addEventListener("click", (e) => {
-                            e.target.disabled=true;
-                            addToNominate(movie);
+                        e.target.disabled = true;
+                        addToNominate(movie);
                     })
 
-                    detailsNode.innerHTML = `${movie.Title}, ${movie.Year}`
+                    detailsNode.innerHTML = `${movie.Title}, ${movie.Year}<br/>`
+
+                    if (checkNominate(movie)) {
+                        console.log("movie already nominated")
+                        btnNode.setAttribute("disabled", "true");
+                    }
 
                     detailsNode.appendChild(btnNode);
                     results.appendChild(detailsNode);
                 })
             }
 
-        }).catch(e => console.log(`Exception caught${e}`));
+        }).catch(e => console.log(`Exception caught ${e}`));
 }
 
 
 
 const addToNominate = (movie) => {
 
-    
 
-    document.querySelector("#nominationList").innerHTML="";
+
+    document.querySelector("#nominationList").innerHTML = "";
     nominatedMovies.push(movie)
     console.log(nominatedMovies);
     nominatedMovies.forEach(movie => {
 
-            let detailsNode = document.createElement("li");
-            detailsNode.innerHTML = `${movie.Title}, ${movie.Year}`;
-            let btnNode = document.createElement("button");
-            btnNode.innerHTML = "Remove";
-            btnNode.setAttribute("id", `${movie.Title}Remove`);
-            btnNode.addEventListener("click", (e) => {
-                e.target.disabled=true;
-                document.querySelector(`#${movie.Title}Add`).disabled=false;
-            
+        let detailsNode = document.createElement("li");
+        let btnNode = document.createElement("button");
+        btnNode.setAttribute("class", "btn btn-danger");
+        btnNode.innerHTML = "Remove";
+        btnNode.setAttribute("id", `${movie.imdbID}Remove`);
+        detailsNode.innerHTML = `${movie.Title}, ${movie.Year} <br/>`;
+
+        btnNode.addEventListener("click", (e) => {
+            e.target.disabled = true;
+            document.querySelector(`#${movie.imdbID}Add`).disabled = false;
+            detailsNode.remove();
+            let removeEl = nominatedMovies.indexOf(movie);
+
+            (removeEl > -1) ? (nominatedMovies.splice(removeEl, 1)) : null;
         })
-
-
-
         detailsNode.appendChild(btnNode);
         document.querySelector("#nominationList").appendChild(detailsNode)
     })
 
-    if (nominatedMovies.length === 5){
+    if (nominatedMovies.length === 5) {
         console.log("All done");
+
     }
 }
 
@@ -86,16 +95,14 @@ const addToNominate = (movie) => {
 
 
 searchBtn.addEventListener("click", () => {
-    results.innerHTML="";
-
+    results.innerHTML = "";
     let link = "http://www.omdbapi.com/?i=tt3896198&apikey=824b361b&s=" + searchDetails.value;
     searchForMovie(link)
 })
 
 searchDetails.addEventListener("keydown", (e) => {
     if (e.key === 'Enter') {
-            
-        results.innerHTML="";
+        results.innerHTML = "";
         let link = "http://www.omdbapi.com/?i=tt3896198&apikey=824b361b&s=" + searchDetails.value;
         searchForMovie(link)
     }
